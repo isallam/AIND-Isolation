9af96ea3e6007cf0255e7130e95497d103f0ecf5
Ibrahim Sallam <ibrahim@objectivity.com>
2017-03-19 22:51:16 -0400

    few fixes and modification for trials heuristic functions

--- game_agent.py
+++ game_agent.py
@@ -21,6 +21,7 @@ only_three_options = [(0,1),(0,5), (1,0), (1,6), (5,0), (5,6), (6,1), (6,5)]
 better_moves = open_book_level1 + open_book_level2
 worse_moves = corner_spots + only_three_options
 
+
 def pick_good_move(legal_moves):
     """ Check if we have a good move in the set of legal moves.
         pick one that is best suitable or return a random one if non
@@ -49,51 +50,6 @@ def pick_good_move(legal_moves):
 
     return legal_moves[random.randint(0, len(legal_moves) - 1)]
 
-def jaccard_score_v2(game, player):
-    """ This score  compare the jaccard similarity of the player legal moves and
-        the better_moves to the the opponent player jaccard similarity
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : hashable
-        One of the objects registered by the game object as a valid player.
-        (i.e., `player` should be either game.__player_1__ or
-        game.__player_2__).
-
-    Returns
-    ----------
-    float
-        The heuristic value of the current game state
-    """
-    if game.is_loser(player):
-        return float("-inf")
-
-    if game.is_winner(player):
-        return float("inf")
-
-    jaccard_score = 0;
-    player_legal_moves = game.get_legal_moves(player)
-    opp_player_legal_moves = game.get_legal_moves(game.get_opponent(player))
-
-    num_intersect_legal_moves = len(list(set(player_legal_moves) & set(better_moves)))
-    num_union_legal_moves = len(player_legal_moves) + len(better_moves)
-    player_jaccard_index = num_intersect_legal_moves / float(num_union_legal_moves)
-
-    num_intersect_legal_moves = len(list(set(opp_player_legal_moves) & set(better_moves)))
-    num_union_legal_moves = len(opp_player_legal_moves) + len(better_moves)
-    opp_player_jaccard_index = num_intersect_legal_moves / float(num_union_legal_moves)
-
-    jaccard_score = player_jaccard_index - opp_player_jaccard_index
-    if jaccard_score > 0:
-        return  jaccard_score
-
-    own_moves = len(player_legal_moves)
-    opp_moves = len(opp_player_legal_moves)
-    return float(own_moves - opp_moves)
 
 def jaccard_score(game, player):
     """ This score measure the jaccard similarity of the player legal moves to
@@ -129,17 +85,13 @@ def jaccard_score(game, player):
     num_union_legal_moves = len(player_legal_moves) + len(opp_player_legal_moves)
     jaccard_index = num_intersect_legal_moves / float(num_union_legal_moves)
 
-    if jaccard_index > 0:
-        return  jaccard_index
-
     own_moves = len(player_legal_moves)
     opp_moves = len(opp_player_legal_moves)
-    return float(own_moves - opp_moves)
+    return float(own_moves - jaccard_index)
 
-def negative_impact_score(game, player):
-    """The "Improved" evaluation function discussed in lecture that outputs a
-    score equal to the difference in the number of moves available to the
-    two players. What we add is a bonus if the own_moves set is in open_book_level1 or open_book_level2
+
+def ratio_score(game, player):
+    """ measure the ration of number own_moves to number of opp_moves
 
     Parameters
     ----------
@@ -158,112 +110,17 @@ def negative_impact_score(game, player):
         The heuristic value of the current game state
     """
     if game.is_loser(player):
-        return float("-inf")
+        return float('-inf')
 
     if game.is_winner(player):
-        return float("inf")
+        return float('inf')
 
-    player_legal_moves = game.get_legal_moves(player)
-    opp_player_legal_moves = game.get_legal_moves(game.get_opponent(player))
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    if opp_moves == 0:
+        return float('inf')
 
-    impact = 0;
-    # check against the wrose locations
-    # if game.move_count > 25:
-    player_worse_moves =  len(list(set(corner_spots) & set(player_legal_moves)))
-    opp_player_worse_moves = len(list(set(corner_spots) & set(opp_player_legal_moves)))
-    impact = opp_player_worse_moves - player_worse_moves
-
-    own_moves = len(player_legal_moves)
-    opp_moves = len(opp_player_legal_moves)
-    return float(own_moves - opp_moves + impact)
-
-def positive_impact_score(game, player):
-    """The "Improved" evaluation function discussed in lecture that outputs a
-    score equal to the difference in the number of moves available to the
-    two players. What we add is a bonus if the own_moves set is in open_book_level1 or open_book_level2
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : hashable
-        One of the objects registered by the game object as a valid player.
-        (i.e., `player` should be either game.__player_1__ or
-        game.__player_2__).
-
-    Returns
-    ----------
-    float
-        The heuristic value of the current game state
-    """
-    if game.is_loser(player):
-        return float("-inf")
-
-    if game.is_winner(player):
-        return float("inf")
-
-    reward = 0;
-    player_legal_moves = game.get_legal_moves(player)
-    opp_player_legal_moves = game.get_legal_moves(game.get_opponent(player))
-
-    if game.move_count <= 20:
-        player_better_moves = len(list(set(open_book_level1) & set(player_legal_moves)))
-        opp_player_better_moves = len(list(set(open_book_level1) & set(opp_player_legal_moves)))
-        reward = (player_better_moves - opp_player_better_moves)
-
-    own_moves = len(player_legal_moves)
-    opp_moves = len(opp_player_legal_moves)
-    return float(own_moves - opp_moves + reward)
-
-
-def impact_score(game, player):
-    """The "Improved" evaluation function discussed in lecture that outputs a
-    score equal to the difference in the number of moves available to the
-    two players. What we add is a bonus if the own_moves set is in open_book_level1 or open_book_level2
-
-    Parameters
-    ----------
-    game : `isolation.Board`
-        An instance of `isolation.Board` encoding the current state of the
-        game (e.g., player locations and blocked cells).
-
-    player : hashable
-        One of the objects registered by the game object as a valid player.
-        (i.e., `player` should be either game.__player_1__ or
-        game.__player_2__).
-
-    Returns
-    ----------
-    float
-        The heuristic value of the current game state
-    """
-    if game.is_loser(player):
-        return float("-inf")
-
-    if game.is_winner(player):
-        return float("inf")
-
-    reward = 0;
-
-    player_legal_moves = game.get_legal_moves(player)
-    opp_player_legal_moves = game.get_legal_moves(game.get_opponent(player))
-
-    num_player_better_moves = len(list(set(open_book_level1) & set(player_legal_moves)))
-    num_opp_player_better_moves = len(list(set(open_book_level1) & set(opp_player_legal_moves)))
-    reward = num_player_better_moves - num_opp_player_better_moves
-
-    # check against the wrose locations
-    player_worse_moves =  len(list(set(corner_spots) & set(player_legal_moves)))
-    opp_player_worse_moves = len(list(set(corner_spots) & set(opp_player_legal_moves)))
-    reward += opp_player_worse_moves - player_worse_moves
-
-
-    own_moves = len(player_legal_moves)
-    opp_moves = len(opp_player_legal_moves)
-    return float(own_moves - opp_moves + reward)
-
+    return float(own_moves) / float(opp_moves)
 
 def yet_another_score(game, player):
     """ Based on the "Improved" evaluation function discussed in lecture but this
@@ -293,9 +150,10 @@ def yet_another_score(game, player):
 
     own_moves = len(game.get_legal_moves(player))
     opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    if opp_moves == 0:
+        return float('inf')
 
-    return float((own_moves - opp_moves) / (own_moves + opp_moves))
-
+    return float(own_moves - opp_moves) / float(own_moves + opp_moves)
 
 
 def improved_score_squared(game, player):
@@ -439,7 +297,6 @@ class CustomPlayer:
         # immediately if there are no legal moves
 
         # Check the open book again the legal_moves and keep one handy
-        # for now we'll just pick a random one from the available moves.
         best_move = pick_good_move(legal_moves)
         if game.move_count <= 1:
             return best_move
@@ -449,20 +306,13 @@ class CustomPlayer:
             # here in order to avoid timeout. The try/except block will
             # automatically catch the exception raised by the search method
             # when the timer gets close to expiring
-            curr_best_move = best_move
-            curr_best_move_stable_count = 0
             depth = 0;
             if self.iterative:
                 while True:
-                    depth +=1
-                    _, curr_best_move = getattr(self, self.method)(game, depth)
-                    if curr_best_move == best_move:
-                        curr_best_move_stable_count+=1
-                    else:
-                        curr_best_move_stable_count = 0
-                        best_move = curr_best_move
-                    if curr_best_move_stable_count > 5:
-                        break
+                    depth += 1
+                    _, good_move = getattr(self, self.method)(game, depth)
+                    if good_move != (-1, -1):
+                        best_move = good_move
             else: # no ID
                 _, best_move = getattr(self, self.method)(game, self.search_depth)
             pass
@@ -472,7 +322,6 @@ class CustomPlayer:
             pass
 
         # Return the best move from the last completed search iteration
-        # print("BestMove: ", best_move)
         return best_move
 
     def minimax(self, game, depth, maximizing_player=True):
@@ -506,6 +355,7 @@ class CustomPlayer:
                 to pass the project unit tests; you cannot call any other
                 evaluation function directly.
         """
+
         if self.time_left() < self.TIMER_THRESHOLD:
             # print("TIMEOUT in minimax()")
             raise Timeout()
@@ -520,8 +370,9 @@ class CustomPlayer:
             game_score = self.score(game, self)
             return (game_score, (-1, -1))
 
+        good_pick = (0, (-1, -1))
+
         # iterate over every potential game and calculate the best score for the branch
-        good_pick = ()
         if maximizing_player:
             good_pick = (float("-inf"), (-1, -1))
             for move in legal_moves:
@@ -529,6 +380,8 @@ class CustomPlayer:
                 branch_score, _ = self.minimax(game_branch, depth -1, (not maximizing_player))
                 if branch_score > good_pick[0]:
                     good_pick = (branch_score, move)
+                if self.time_left() < self.TIMER_THRESHOLD:
+                    break
         else:
             good_pick = (float("+inf"), (-1, -1))
             for move in legal_moves:
@@ -536,6 +389,8 @@ class CustomPlayer:
                 branch_score, _ = self.minimax(game_branch, depth -1, (not maximizing_player))
                 if branch_score < good_pick[0]:
                     good_pick = (branch_score, move)
+                if self.time_left() < self.TIMER_THRESHOLD:
+                    break
 
         return good_pick
 
@@ -591,12 +446,14 @@ class CustomPlayer:
             game_score = self.score(game, self)
             return (game_score, (-1, -1))
 
+        global gDepth
         good_choice = ()
         if maximizing_player:
             best_score = alpha
             good_choice = (alpha, (-1, -1))
             for move in legal_moves:
                 game_branch = game.forecast_move(move)
+                gDepth = depth
                 branch_score, _ = self.alphabeta(game_branch, depth -1, alpha, beta, (not maximizing_player))
                 if branch_score > best_score:
                     good_choice = (branch_score, move)
